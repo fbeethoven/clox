ebf9e75b0951580ad98c4b20964b06a6a7e31d38
fbeethoven <fulgencio.beethoven@gmail.com>
2024-08-29 19:31:44 +0200

    feat(chapter15): value stack

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 
 int main(int argc, const char *argv[]) {
+    init_vm();
     Chunk chunk;
     init_chunk(&chunk);
 
@@ -13,7 +15,9 @@ int main(int argc, const char *argv[]) {
     write_chunk(&chunk, OP_RETURN, 123);
 
     disassemble_chunk(&chunk, "test chunk");
+    interpret(&chunk);
 
+    free_vm();
     free_chunk(&chunk);
 
     return 0;
