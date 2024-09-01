dfaa14ecfd28f0f52e9e50bc2f350062165089c8
fbeethoven <fulgencio.beethoven@gmail.com>
2024-09-01 09:57:53 +0200

    feat(chapter21): add global variables

--- scanner.c
+++ scanner.c
@@ -150,7 +150,7 @@ static TokenType identifier_type() {
                 }
             }
             break;
-        case 'v': return check_keyword(1, 2, "uper", TOKEN_VAR);
+        case 'v': return check_keyword(1, 2, "ar", TOKEN_VAR);
         case 'w': return check_keyword(1, 4, "hile", TOKEN_WHILE);
     }
     return TOKEN_IDENTIFIER;
