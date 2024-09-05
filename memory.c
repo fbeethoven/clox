60a917cb800a0762fb8c62e708b4271f4184d057
fbeethoven <fulgencio.beethoven@gmail.com>
2024-09-05 10:11:02 +0200

    feat(chapter26): implement Garbage Collection

--- memory.c
+++ memory.c
@@ -10,11 +10,20 @@
 #endif
 
 
+#define GC_HEAP_GROW_FACTOR 2
+
+
 void *reallocate(void *pointer, size_t old_size, size_t new_size) {
+    vm.bytes_allocated += new_size - old_size;
+
     if (new_size > old_size) {
         #ifdef DEBUG_STRESS_GC
             collect_garbage();
         #endif
+    }
+
+    if (vm.bytes_allocated > vm.next_gc) {
+        collect_garbage();
     }
 
     if (new_size == 0) {
@@ -193,6 +202,7 @@ static void sweep() {
 void collect_garbage() {
     #ifdef DEBUG_LOG_GC
         printf("-- gc begin\n");
+        size_t before = vm.bytes_allocated;
     #endif
 
     mark_roots();
@@ -200,8 +210,13 @@ void collect_garbage() {
     table_remove_white(&vm.strings);
     sweep();
 
+    vm.next_gc = vm.bytes_allocated * GC_HEAP_GROW_FACTOR;
+
     #ifdef DEBUG_LOG_GC
         printf("-- gc end\n");
+        printf("   collected %zu bytes (from %zu to %zu) next at %zu\n",
+            before - vm.bytes_allocated, before, vm.bytes_allocated, vm.next_gc
+        );
     #endif
 }
 
